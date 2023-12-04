b1fc5096e752a587272d11a7fd1d4c206e01ca42
Sergey <ivolginsa@gmail.com>
2023-12-04 14:41:12 +0000

    Add custom decorator

--- repository_template/src/tg_bot/decorators.py
+++ repository_template/src/tg_bot/decorators.py
@@ -1,11 +1,13 @@
 from typing import Type, Any
 from yostate import Locator
+from tg_api import SendMessageRequest
 
 from django_tg_bot_framework import (
     PrivateChatState,
     PrivateChatMessageReceived,
 )
 
+from .models import Conversation
 
 def redirect_menu_commands(state_class: Type[PrivateChatState]) -> Type[PrivateChatState]:
     class WrappedStateClass(state_class):
@@ -20,4 +22,16 @@ def redirect_menu_commands(state_class: Type[PrivateChatState]) -> Type[PrivateC
                         return Locator('/welcome/')
 
             return super().process(event=event)
+    return WrappedStateClass
+
+
+def announce_locator(state_class: Type[PrivateChatState]) -> Type[PrivateChatState]:
+    class WrappedStateClass(state_class):
+        def enter_state(self) -> Locator | None:
+            locator = self.locator.state_class_locator
+            SendMessageRequest(
+                text=f'Новый локатор: {locator}',
+                chat_id=Conversation.current.tg_chat_id
+            ).send()
+            return super().enter_state()
     return WrappedStateClass
